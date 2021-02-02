c8cc7037a9e470fe285daae163f37c9d4ecbf39f
juvenalbruno <bruno.andrade178@hotmail.com>
2021-02-02 19:02:04 -0300

    master

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -2,11 +2,22 @@ import React from 'react';
 import Sidebar from '../components/Sidebar';
 import '../styles/portfolio.css';
 
+import Quiz from '../assets/quiz.png';
+import Ecoleta from '../assets/ecoleta.png';
+import Happy from '../assets/happy.png';
+import Juflix from '../assets/juflix.png';
+
 export default function Portfolio() {
     return (
         <div id="page-portfolio">
             <div className="sidebar">
                 <Sidebar />
+            </div>
+            <div className="container">
+                <img src={Quiz} alt="quiz"/>
+                <img src={Juflix} alt="Juflix"/>
+                <img src={Ecoleta} alt="Ecoleta"/>
+                <img src={Happy} alt="Happy"/>
             </div>
         </div>
     )
