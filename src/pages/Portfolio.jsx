c7dc3dca9023bbd9a985baa9cb50bbf6ce6046e4
juvenalbruno <bruno.andrade178@hotmail.com>
2021-02-06 11:58:25 -0300

    master

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,27 +1,99 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Sidebar from '../components/Sidebar';
 import '../styles/portfolio.css';
-import { Link } from 'react-router-dom';
 
+import CapaQuiz from '../assets/CapaQuiz.png';
 import Quiz from '../assets/Quiz.png';
+import CapaEcoleta from '../assets/CapaEcoleta.png';
 import Ecoleta from '../assets/Ecoleta.png';
+import CapaHappy from '../assets/CapaHappy.png';
 import Happy from '../assets/Happy.png';
+import CapaJuflix from '../assets/CapaJuflix.png';
 import Juflix from '../assets/Juflix.png';
+import CapaBeTheHero from '../assets/CapaBeTheHero.png';
 import BeTheHero from '../assets/BeTheHero.png';
 
+import { FaArrowCircleLeft } from 'react-icons/fa';
+
 export default function Portfolio() {
+    const [Visibility, setVisibility] = useState(false);
+    const [imgId, setImgId] = useState();
+
     return (
         <div id="page-portfolio">
             <div className="sidebar">
                 <Sidebar />
             </div>
-            <div className="container">
-                <Link to="/portfolio/quiz"><img src={Quiz} alt="Quiz"/></Link>
-                <Link to="/portfolio/juflix"><img src={Juflix} alt="Juflix"/></Link>
-                <Link to="/portfolio/ecoleta"><img src={Ecoleta} alt="Ecoleta"/></Link>
-                <Link to="/portfolio/happy"><img src={Happy} alt="Happy"/></Link>
-                <Link to="/portfolio/bethehero"><img src={BeTheHero} alt="Be The hero"/></Link>
+
+            <div className="modal" style={Visibility ? { visibility:'visible' } : { visibility: 'hidden' }}>
+                <div className="modal_content">
+                    <img src={imgId} alt="" className="modal_img"/>
+                </div>
+                <button 
+                    className="btn_close" 
+                    onClick={() => {
+                        setVisibility(false)
+                        setImgId('')
+                    }}
+                >
+                    <FaArrowCircleLeft size={20} />
+                    Voltar
+                </button>
             </div>
+            
+            <div className="container">
+                <div className="card_img">
+                    <img
+                        src={CapaQuiz}
+                        alt="Quiz"
+                        onClick={() => {
+                            setVisibility(true)
+                            setImgId(Quiz)
+                        }}
+                    />
+                </div>
+                <div className="card_img">
+                    <img
+                        src={CapaJuflix}
+                        alt="Juflix"
+                        onClick={() => {
+                            setVisibility(true)
+                            setImgId(Juflix)
+                        }}
+                    />
+                </div>
+                <div className="card_img">
+                    <img
+                        src={CapaEcoleta}
+                        alt="Ecoleta"
+                        onClick={() => {
+                            setVisibility(true)
+                            setImgId(Ecoleta)
+                        }}
+                    />
+                </div>
+                <div className="card_img">
+                    <img
+                        src={CapaHappy}
+                        alt="Happy"
+                        onClick={() => {
+                            setVisibility(true)
+                            setImgId(Happy)
+                        }}
+                    />
+                </div>
+                <div className="card_img">
+                    <img
+                        src={CapaBeTheHero}
+                        alt="Be The hero"
+                        onClick={() => {
+                            setVisibility(true)
+                            setImgId(BeTheHero)
+                        }}
+                    />
+                </div>
+            </div>
+
         </div>
     )
 }
--- src/routes.jsx
+++ src/routes.jsx
@@ -5,11 +5,6 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Portfolio from './pages/Portfolio';
 import Contact from './pages/Contact';
-import Happy from './pages/extensions/Happy';
-import Ecoleta from './pages/extensions/Ecoleta';
-import Juflix from './pages/extensions/Juflix';
-import Quiz from './pages/extensions/Quiz';
-import BeTheHero from './pages/extensions/BeTheHero';
 
 export default function Routes(){
     return(
@@ -17,12 +12,7 @@ export default function Routes(){
           <Switch>
             <Route path="/" component={Home} exact/>
             <Route path="/about" component={About} />
-            <Route path="/portfolio" component={Portfolio} exact/>
-            <Route path="/portfolio/happy" component={Happy} />
-            <Route path="/portfolio/ecoleta" component={Ecoleta} />
-            <Route path="/portfolio/juflix" component={Juflix} />
-            <Route path="/portfolio/quiz" component={Quiz} />
-            <Route path="/portfolio/bethehero" component={BeTheHero} /> 
+            <Route path="/portfolio" component={Portfolio} />
             <Route path="/contact" component={Contact} />
           </Switch>
         </BrowserRouter>
